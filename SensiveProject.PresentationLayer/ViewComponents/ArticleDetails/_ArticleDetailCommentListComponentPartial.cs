953f49d362be2415133812c44f607da2c6bb815d
MuratYucedag <yucedagmurat23@gmail.com>
2024-11-26 14:57:19 +0300

    ...

---
+++ SensiveProject.PresentationLayer/ViewComponents/ArticleDetails/_ArticleDetailCommentListComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveProject.BusinessLayer.Abstract;
+
+namespace SensiveProject.PresentationLayer.ViewComponents.ArticleDetails
+{
+    public class _ArticleDetailCommentListComponentPartial : ViewComponent
+    {
+        private readonly ICommentService _commentService;
+        public _ArticleDetailCommentListComponentPartial(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var value = _commentService.TGetCommentsByArticleId(1);
+            return View(value);
+        }
+    }
+}
